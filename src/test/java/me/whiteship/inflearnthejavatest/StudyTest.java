0f7c6aa3c6d0e9ffbeb3953fb53ff97e79d283b9
Dong-wook94 <gygacpu@naver.com>
2022-01-05 23:35:28 +0900

    02. JUnit 시작하기.

--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -19,21 +19,25 @@ class StudyTest {
         System.out.println("create1");
     }
 
+    //모든 메소드(테스트)가 실행되기 전에 호출.
     @BeforeAll
     static void beforeAll() {
         System.out.println("before all");
     }
 
+    //모든테스트가 실행된 이후 딱 한번만 호출.
     @AfterAll
     static void afterAll() {
         System.out.println("after all");
     }
 
+    //각각의 test method가 실행될때 호출 , method 실행 전에 호출
     @BeforeEach
     void beforeEach() {
         System.out.println("Before each");
     }
 
+    //각각의 test method가 실행될때 호출, method 실행 후에 호출
     @AfterEach
     void afterEach() {
         System.out.println("After each");
